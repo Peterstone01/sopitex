cf076a79c408fada8c441aac86f07012f0dc067b
peterstonw01 <peterogechukwu1@gmail.com>
2025-08-16 10:04:10 +0100

    hero image change

--- src/Carousel.jsx
+++ src/Carousel.jsx
@@ -3,12 +3,19 @@ import React from "react";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
-import room1 from "./assets/room1.jpg";
-import room7 from "./assets/room7.jpg";
-import d1 from "./assets/d1.jpg";
-import d2 from "./assets/d2.jpg";
-import d4 from "./assets/d4.jpg";
-import d5 from "./assets/d5.jpg";
+import home1 from "./assets/home1.jpg";
+import home2 from "./assets/home2.jpg";
+import home3 from "./assets/home3.jpg";
+import home4 from "./assets/home4.jpg";
+import home5 from "./assets/home5.jpg";
+import home6 from "./assets/home6.jpg";
+import home7 from "./assets/home7.jpg";
+import home8 from "./assets/home8.jpg";
+import home9 from "./assets/home9.jpg";
+import home10 from "./assets/home10.jpg";
+import home11 from "./assets/home11.jpg";
+import home12 from "./assets/home12.jpg";
+import home13 from "./assets/home13.jpg";
 
 ////////////////////////////////////////// arrows (not used anywere)
 function SampleNextArrow(props) {
@@ -36,7 +43,21 @@ function SamplePrevArrow(props) {
 //////////////////////////////////////////end of arrows
 
 const Carousel = () => {
-  const slides = [d1, d2, room7, d4, d5, room1];
+  const slides = [
+    home1,
+    home2,
+    home3,
+    home4,
+    home5,
+    home6,
+    home7,
+    home8,
+    home9,
+    home10,
+    home11,
+    home12,
+    home13,
+  ];
   var settings = {
     // dots: true,
     infinite: true,
@@ -57,18 +78,18 @@ const Carousel = () => {
     <div className="slider-container scrollbar-hide  mx-auto relative">
       <Slider {...settings} className="relative">
         {slides.map((item, i) => (
-          <div key={i} className="h-[50vh] md:h-[60vh] lg:h-[80vh]  w-full ">
+          <div key={i} className="h-[50vh] md:h-[60vh] lg:h-[100vh]  w-full ">
             <img
               src={item}
               alt="imaes"
-              className="h-[50vh] md:h-[60vh] lg:h-[80vh] w-full object-cover"
+              className="h-[50vh] md:h-[60vh] lg:h-[100vh] w-full object-cover"
             />
           </div>
         ))}
       </Slider>
       {/* ///////////////overlay */}
       {/* ///////////////////// for mid size screen and above */}
-      <div className="  w-full opacity-60  mt-36  lg:mt-20 h-[300px] lg:h-[220px] lg:py-16 py-10  bg-slate-900 absolute z-20  top-1/4  -translate-y-1/2"></div>{" "}
+      <div className="  w-full opacity-60     h-full lg:py-16 py-10  bg-slate-900 absolute z-20  top-0 left-0"></div>{" "}
       <div className="hidden  absolute  top-2/4 mt-36 lg:mt-20 w-full -translate-y-1/2 z-20 px-5 py-10">
         <h1 className="text-2xl md:text-5xl   text-primary font-bold  z-20  pt-14 mb-5">
           Redefining Entertainment &
@@ -96,12 +117,12 @@ const Carousel = () => {
       {/* <div className="absolute w-full h-full top-0 left-0 bg-slate-900 opacity-70 "></div> */}
       {/* /////contents for small screen hero */}
       <div className="absolute mt-20 md:-mt-20 inset-0 flex flex-col items-center justify-center  ">
-        <h1 className=" text-4xl md:text-5xl text-center   text-primary font-bold  z-20   ">
+        <h1 className=" text-2xl md:text-5xl text-center   text-white font-bold  z-20   ">
           Redefining Entertainment &
           <span className=""> Hospitality in Nigeria </span>
         </h1>
         <div>
-          <p className="   text-white my-2  font-bold text-center ">
+          <p className="   text-gray-200 my-2   text-center ">
             Where Family Fun Meets Nightlife, Tech & Premium Stays
           </p>
         </div>
--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -43,11 +43,11 @@ const Services = () => {
       </h2>
       <div className="px-2 bg-gray-200 py-10">
         {/* ////////////////////////Tabs (button) */}
-        <div className="flex justify-center items-center gap-2 md:gap-5 w-full mx-auto ">
+        <div className="flex justify-center items-center gap-1 md:gap-5 w-full mx-auto ">
           {tabs.map((tab) => (
             <div key={tab.id} className=" ">
               <button
-                className={` rounded-lg font-bold text-white px-2 md:px-3 py-2 ${
+                className={` rounded-lg font-bold text-white px-1 md:px-3 py-2 ${
                   activeTab === tab.id
                     ? " bg-primary text-secondary"
                     : " bg-slate-900"
